d217ad1c348d3d2a80eb68e64c87249d82b7f107
lukak85 <luka.kuzman85@gmail.com>
2021-12-28 20:51:17 +0100

    Fixed maven dependencies

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,1 +1,1 @@
-INSERT INTO customer (username, email, password, firstName, lastName, yearBorn, funds, charging) VALUES ('testuser1', 'test@test.si', 'password1234', 'User', 'Bloke', 1998, 1234.5, false);
+INSERT INTO customer (username, email, password, firstName, lastName, yearBorn, funds, charging) VALUES ('testuser1', 'test@test.si', 'pw1234', 'User', 'Bloke', 1998, 1234.5, false);
